5383c69ecc73f0bd5d26d033b8f091293055dacb
yasmine <yasmineelmezouari23@augustana.edu>
2024-10-12 14:19:18 -0500

    Worked on initialize for LiveHamController

--- HAMradio/src/main/java/edu/augustana/LiveHamController.java
+++ HAMradio/src/main/java/edu/augustana/LiveHamController.java
@@ -12,5 +12,27 @@ public class LiveHamController {
     @FXML private Slider frequencySlider;
     @FXML private Slider filterSlider;
 
+    private String frequencyUnit = " Mhz";
+    private final int minFrequency = 200;
+    private final int maxFrequency = 500;
+    private final int initialFrequency = 350;
+
+
+
+    public void initialize() {
+        frequencySlider.setMin(minFrequency);
+        frequencySlider.setMax(maxFrequency);
+        frequencySlider.setValue(initialFrequency);
+       chosenFrequency.setText(Double.toString(frequencySlider.getValue()) + frequencyUnit);
+       userMessage.setText("Your message will be shown here");
+
+        frequencySlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            chosenFrequency.setText(String.format("%.2f", newValue) + frequencyUnit);
+        });
+
+
+    }
+
+
 
 }
