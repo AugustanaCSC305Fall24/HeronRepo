ab305ab48390b9d2bdfbede20785e0426d89f6df
yasmine <yasmineelmezouari23@augustana.edu>
2024-11-03 15:35:36 -0600

    Worked more on the Scenario menu

--- HAMradio/src/main/java/edu/augustana/Scenarios/ScenarioMenuController.java
+++ HAMradio/src/main/java/edu/augustana/Scenarios/ScenarioMenuController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
 
+import javax.swing.text.html.ImageView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,14 @@ public class ScenarioMenuController {
 
     @FXML
     private Slider transmissionSpeed;
+
+    @FXML
+    private ImageView saveScenario;
+
+    @FXML
+    private ImageView openScenario;
+
+
 
     private List<ScenarioBot> listOfBots = new ArrayList<>();
 
