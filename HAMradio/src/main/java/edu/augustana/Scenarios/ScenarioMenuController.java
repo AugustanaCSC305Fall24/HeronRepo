f0477d6b6f7daf9f605bd6cd10a0d9713443ef2a
yasmine <yasmineelmezouari23@augustana.edu>
2024-11-03 15:50:55 -0600

    Fixed issue with opening practice scenario window

--- HAMradio/src/main/java/edu/augustana/Scenarios/ScenarioMenuController.java
+++ HAMradio/src/main/java/edu/augustana/Scenarios/ScenarioMenuController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
 
-import javax.swing.text.html.ImageView;
+import javafx.scene.image.ImageView;
 import java.util.ArrayList;
 import java.util.List;
 
